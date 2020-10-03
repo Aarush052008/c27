83031b3baea23d91634f1870c120d58a1ebf9c87
Aarush052008 <aarushg052008@gmail.com>
2020-10-03 10:39:07 +0530

    created a constrained object

---
+++ chain.js
@@ -0,0 +1,27 @@
+class Chain{
+    constructor(body1, body2){
+       var options = {
+        bodyA: body1,
+        bodyB: body2,
+        stiffness: 0.04,
+        length: 10
+       }
+
+       this.chain = Constraint.create(options);
+       World.add(world,this.chain);
+       console.log(this.chain);
+    }
+
+
+    display(){
+        var posA = this.chain.bodyA.position;
+        var posB = this.chain.bodyB.position;
+        strokeWeight(4);
+        stroke("brown");
+        line(posA.x,posA.y,posB.x,posB.y);
+
+
+
+    }
+
+}
